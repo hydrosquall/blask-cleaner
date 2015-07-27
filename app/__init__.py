01629be2247009ea7ba64761d71852a874dbe509
Cameron Yick <cameron.yick@yale.edu>
2015-07-27 14:58:35 -0400

    refactor app into scalable structure

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+# Import and register the Blueprints
+from app.routes.blog import blog
+from app.routes.home import home
+app.register_blueprint(blog)
+app.register_blueprint(home)
+
+# Because our routes are now spread across multiple modules, we need to use the Blueprint name to specify which module the desired route lives in. The string inside a call to url_for is blueprint_name.function_name. We can very easily update home.html to reflect this
+# OLD: {{ url_for('home_page') }}
+# NEW: {{ url_for('home.home_page') }}
---
+++ structure.md
@@ -0,0 +1,12 @@
+## File Structure ##
+
+├── app/                # Anything connected to the webapp
+│   ├── __init__.py     # Core application logic
+│   ├── routes/         # Routing
+│   │   └── __init__.py # Empty file
+│   ├── static/         # Static content
+│   │   ├── css/        # CSS stylesheets
+│   │   ├── img/        # Images
+│   │   └── js/         # JavaScript files
+│   └── templates/      # HTML pages
+└── run.py              # The boot script
